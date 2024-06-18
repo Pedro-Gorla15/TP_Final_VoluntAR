f2289a37d53173ed0a637dd6745e07ac58ce21eb
Pedro Gorla <pedrogorla15@gmail.com>
2024-06-17 22:23:27 -0300

    Modificación del archivo ancianos.js de la carpeta servicio. Se importó la función validar de la carpeta validaciones para corregir el formulario de index.html que guardaba la edad entre comillas como un string dentro del json del navegador. Ahora se guarda como un número.

--- servicio/ancianos.js
+++ servicio/ancianos.js
@@ -2,7 +2,6 @@ import ModelFactory from '../model/DAOs/entidadesFactory.js'
 import config from '../config.js'
 import { validar } from './validaciones/ancianos.js'
 
-
 class ServicioAncianos {
     constructor() {
         this.model = ModelFactory.get(config.MODO_PERSISTENCIA, 'ancianos')
@@ -21,16 +20,17 @@ class ServicioAncianos {
     guardarAnciano = async anciano => {
         const res = validar(anciano)
         if (res.result) {
+            anciano.edad = Number(anciano.edad)
             const ancianoGuardado = await this.model.guardarAnciano(anciano)
             return ancianoGuardado
-        }
-        else {
+        } else {
             console.log(res.error)
             throw res.error
         }
     }
 
     actualizarAnciano = async (id, anciano) => {
+        anciano.edad = Number(anciano.edad)
         const ancianoActualizado = await this.model.actualizarAnciano(id, anciano)
         return ancianoActualizado
     }
